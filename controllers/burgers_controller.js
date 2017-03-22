6d7bc8c1c4965c02d42a875b62fda0ee15cab9c0
Ivonne Komis <ivonnekomis@Ivonnes-MBP.fios-router.home>
2017-03-22 14:42:53 -0400

    all logic works

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,9 +17,9 @@ router.get("/", function(req, res) {
 
 router.post("/", function(req, res) {
   burger.create([
-    "name", "devoured"
+    "burger_name"
   ], [
-    req.body.name, req.body.devoured
+    req.body.burger_name
   ], function() {
     res.redirect("/");
   });
@@ -31,7 +31,7 @@ router.put("/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    name: req.body.devoured
+    devoured: req.body.devoured
   }, condition, function() {
     res.redirect("/");
   });
