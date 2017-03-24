66f4742919ad4d955c34df7a17e79474de9c340a
Ivonne Komis <ivonnekomis@Ivonnes-MBP.fios-router.home>
2017-03-24 15:51:53 -0400

    chg json and social links

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.port || 8080;
+var port = process.env.port || 3000;
 
 var app = express();
 
