27a1ef1c7c2b5a7362678870c8fa506e61b8d2b8
Ivonne Komis <ivonnekomis@Ivonnes-MacBook-Pro.local>
2017-03-25 09:52:52 -0400

    upd connection hard code heroku connection

--- config/connection.js
+++ config/connection.js
@@ -5,17 +5,26 @@ console.log('connection.js');
 var PORT = process.env.PORT || 3306;
 console.log(PORT);
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     var connection = mysql.createConnection({
+//       port: PORT,
+//       host: "localhost",
+//       user: "root",
+//       password: 'root',
+//       database: "burgers_db"
+//     });
+// };
+
     var connection = mysql.createConnection({
-      port: PORT,
-      host: "localhost",
-      user: "root",
-      password: 'root',
-      database: "burgers_db"
+      port: 3306,
+      host: "y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "ces9iyuv5umtp5c8",
+      password: 'mu7h5qdnify7oa6z',
+      database: "pbvswpiz5xuii725"
     });
-};
+
 
 // Make connection.
 connection.connect(function(err) {
