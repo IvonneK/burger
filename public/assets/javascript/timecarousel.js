3075bfc2b88f220d7df654a34d772bcc13fdc878
Ivonne Komis <ivonnekomis@Ivonnes-MBP.fios-router.home>
2017-03-30 19:10:41 -0400

    added separate js file and added different font

--- public/assets/javascript/timecarousel.js
+++ public/assets/javascript/timecarousel.js
@@ -1,5 +1,3 @@
-// couldn't get it to work with the carousel it wants this code with the jquery script otherwise won't run I left in handlebars.
-
 $(document).ready(function() {
    $('.item').each(function() {
        if (!$(this).hasClass("active")) {
